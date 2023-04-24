c0f9865cb5a67125ef53abf3275c51923454cd5d
M0-C0NTENT <131573588+M0-C0NTENT@users.noreply.github.com>
2023-04-24 08:25:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,65 @@
-const urlInput = document.getElementById("urlInput");
-const generateBtn = document.getElementById("generateBtn");
-const qrCodeDiv = document.getElementById("qrCode");
-const downloadContainer = document.getElementById("downloadContainer");
 
-function generateQRCode() {
-    const url = urlInput.value.trim();
+ @@ -4,40 +4,28 @@ const qrCodeDiv = document.getElementById("qrCode");
+ const downloadContainer = document.getElementById("downloadContainer");
 
-    if (url === "") {
-        alert("Please enter a valid URL.");
-        return;
-    }
+ function generateQRCode() {
+ 	const url = urlInput.value;
 
-    qrCodeDiv.innerHTML = "";
+ 	if (url.trim() === "") {
+ 		alert("Please enter a valid URL.");
+ 		return;
+ 	}
 
-    const qrCode = new QRCode(qrCodeDiv, {
-        text: url,
-        width: 256,
-        height: 256,
-        colorDark: "#000000",
-        colorLight: "#ffffff",
-        correctLevel: QRCode.CorrectLevel.H
-    });
+ 	qrCodeDiv.innerHTML = "";
 
-    const canvas = qrCode._el.firstChild;
-    canvas.toBlob(function(blob) {
-        saveAs(blob, "qrcode.png");
-    });
-}
+ 	const qrCode = new QRCode(qrCodeDiv, {
+ 		text: url,
+ 		width: 256,
+ 		height: 256,
+ 		colorDark: "#000000",
+ 		colorLight: "#ffffff",
+ 		correctLevel: QRCode.CorrectLevel.H
+ 	});
 
-generateBtn.addEventListener("click", generateQRCode);
+ 	const canvas = qrCode._el.firstChild;
+ 	const dataURL = canvas.toDataURL();
+
+ 	const downloadLink = document.createElement("a");
+ 	downloadLink.href = dataURL;
+ 	downloadLink.download = "qrcode.png";
+ 	downloadLink.textContent = "Download QR Code";
+ 	downloadContainer.innerHTML = "";
+ 	downloadContainer.appendChild(downloadLink);
+
+ 	function downloadQRCode() {
+ 		canvas.toBlob(function(blob) {
+ 			saveAs(blob, "qrcode.png");
+ 		});
+ 	}
+ 	downloadLink.addEventListener("click", downloadQRCode);
+     const url = urlInput.value.trim();
+
+     if (url === "") {
+         alert("Please enter a valid URL.");
+         return;
+     }
+
+     qrCodeDiv.innerHTML = "";
+
+     const qrCode = new QRCode(qrCodeDiv, {
+         text: url,
+         width: 256,
+         height: 256,
+         colorDark: "#000000",
+         colorLight: "#ffffff",
+         correctLevel: QRCode.CorrectLevel.H
+     });
+
+     const canvas = qrCode._el.firstChild;
+     canvas.toBlob(function(blob) {
+         saveAs(blob, "qrcode.png");
+     });
+ }
+
+ generateBtn.addEventListener("click
+ generateBtn.addEventListener("click", generateQRCode
