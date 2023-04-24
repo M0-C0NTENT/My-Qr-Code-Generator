75a042b4bc84a227945122611ad50cfbb3b988e5
M0-C0NTENT <131573588+M0-C0NTENT@users.noreply.github.com>
2023-04-24 07:45:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
 const urlInput = document.getElementById("urlInput");
-		const generateBtn = document.getElementById("generateBtn");
-		const qrCodeDiv = document.getElementById("qrCode");
-		const downloadContainer = document.getElementById("downloadContainer");
+const generateBtn = document.getElementById("generateBtn");
+const qrCodeDiv = document.getElementById("qrCode");
+const downloadContainer = document.getElementById("downloadContainer");
 
-		function generateQRCode() {
-			const url = urlInput.value;
+function generateQRCode() {
+	const url = urlInput.value;
 
-			if (url.trim() === "") {
-				alert("Please enter a valid URL.");
-				return;
-			}
-			function downloadQRCode() {
-  var canvas = document.getElementById("qrCodeCanvas");
-  canvas.toBlob(function(blob) {
-    saveAs(blob, "qrcode.png");
-  });
+	if (url.trim() === "") {
+		alert("Please enter a valid URL.");
+		return;
+	}
+
+	qrCodeDiv.innerHTML = "";
+
+	const qrCode = new QRCode(qrCodeDiv, {
+		text: url,
+		width: 256,
+		height: 256,
+		colorDark: "#000000",
+		colorLight: "#ffffff",
+		correctLevel: QRCode.CorrectLevel.H
+	});
+
+	const canvas = qrCode._el.firstChild;
+	const dataURL = canvas.toDataURL();
+
+	const downloadLink = document.createElement("a");
+	downloadLink.href = dataURL;
+	downloadLink.download = "qrcode.png";
+	downloadLink.textContent = "Download QR Code";
+	downloadContainer.innerHTML = "";
+	downloadContainer.appendChild(downloadLink);
+
+	function downloadQRCode() {
+		canvas.toBlob(function(blob) {
+			saveAs(blob, "qrcode.png");
+		});
+	}
+	downloadLink.addEventListener("click", downloadQRCode);
 }
 
-
-			qrCodeDiv.innerHTML = "";
-
-			const qrCode = new QRCode(qrCodeDiv, {
-				text: url,
-				width: 256,
-				height: 256,
-				colorDark: "#000000",
-				colorLight: "#ffffff",
-				correctLevel: QRCode.CorrectLevel.H
-			});
-
-			const canvas = qrCode._el.firstChild;
-			const dataURL = canvas.toDataURL();
-
-			const downloadLink = document.createElement("a");
-			downloadLink.href = dataURL;
-			downloadLink.download = "qrcode.png";
-			downloadLink.textContent = "Download QR Code";
-			downloadContainer.innerHTML = "";
-			downloadContainer.appendChild(downloadLink);
-		}
-
-		generateBtn.addEventListener("click", generateQRCode);
+generateBtn.addEventListener("click
